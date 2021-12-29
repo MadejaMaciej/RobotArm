64950f1d56daa2227eed4c728c41a8ac3b588c2c
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2021-12-29 17:49:13 +0100

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//
+// !!!IMPORTANT!!!
+//
+// Modified/changed for educational purposes by Maciej Madejczyk, Jakub Piechuła and Kacper Zwyrtek.
+//
+// We are using this software and Kinect overall to get bones and joints and send them to our server to steer an robot arm.
+// We didn't write whole code by ourselves because it will be much slower to make. We are not making any profit by using this software and we are only learning it. 
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
